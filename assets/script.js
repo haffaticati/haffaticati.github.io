38d100dd620b9a77126f4be7a7e01b7b515226b3
Hugo Affaticati <haffaticati@microsoft.com>
2025-06-03 22:32:28 -0700

    first deployment

---
+++ assets/script.js
@@ -0,0 +1,6 @@
+document.addEventListener("DOMContentLoaded", function () {
+    document.querySelector(".menu-toggle").addEventListener("click", function () {
+        document.querySelector(".dropdown-menu").classList.toggle("show");
+    });
+});
+
